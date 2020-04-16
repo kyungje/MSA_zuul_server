cc43b449faa05e99e104045bab168eff12bc396e
kyungje <kyungje@gmail.com>
2020-04-16 16:18:37 +0900

    init

---
+++ msa-architecture-zuul-server/src/main/java/com/sk/MsaArchitectureZuulServerApplication.java
@@ -0,0 +1,22 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class MsaArchitectureZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaArchitectureZuulServerApplication.class, args);
+	}
+	
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+}
